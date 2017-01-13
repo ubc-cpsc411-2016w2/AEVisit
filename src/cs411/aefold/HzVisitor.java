732682951069c5be5f8c3bf8bd61f5af8df772c0
Ronald Garcia <ron.garcia@gmail.com>
2017-01-12 20:06:28 -0800

    New Use of Visitor.

--- src/cs411/aefold/HzVisitor.java
+++ src/cs411/aefold/HzVisitor.java
@@ -6,17 +6,17 @@ package cs411.aefold;
 public class HzVisitor implements Visitor<Boolean> {
     @Override
     public Boolean visit(Num nm) {
-        return new Boolean(nm.n == 0);
+        return nm.n == 0;
     }
 
     @Override
     public Boolean visit(Add ad) {
         // Thanks to short-circuiting || it recurs on rhs only if lhs yields false
-        return new Boolean(ad.lhs.accept(this) || ad.rhs.accept(this));
+        return ad.lhs.accept(this) || ad.rhs.accept(this);
     }
 
     @Override
     public Boolean visit(Sub sb) {
-        return new Boolean(sb.lhs.accept(this) || sb.rhs.accept(this));
+        return sb.lhs.accept(this) || sb.rhs.accept(this);
     }
 }
